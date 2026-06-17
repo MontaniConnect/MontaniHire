eda75dfdb0361e994abfca47bc24fe3ccf4d4baf
MontaniConnect <connect@montani.ph>
2026-06-17 16:03:54 +0800

    Add empty credentials to prevent Rails error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE']
 
 Rails.application.configure do
 
+  config.secret_key_base = ENV.fetch('SECRET_KEY_BASE') { SecureRandom.hex(64) }
 config.active_storage.service = :amazon
 
 
